6f1cef7d0fd75b64ec8f6e4f27b73971e6346e96
pierre-deve <pierre.fievet1@ens.univ-artois.fr>
2019-01-25 10:16:49 +0100

    question 7-

--- src/Form/Type/ScoreType.php
+++ src/Form/Type/ScoreType.php
@@ -14,6 +14,7 @@ class ScoreType extends AbstractType
             'choices' => [
                 0,1,2,3,4,5,6,7,8,9,10,11,12
             ],
+            'attr' => ['onChange' => 'changeColor(this.value)']
         ]);
 
     }
