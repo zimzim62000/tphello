7abfd69dbfb2abb537d0e9d907f6303d1d446290
pierre-deve <pierre.fievet1@ens.univ-artois.fr>
2019-01-25 10:00:27 +0100

    question 3- >>> dernier fixe <<<

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -4,9 +4,11 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use App\Validator as AssertAcme;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\GameRepository")
+ * @AssertAcme\ContrainteEquipe
  */
 class Game
 {
