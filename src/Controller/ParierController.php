b4757d2bcbf810a225fab5a6f249b161b438f5c4
Hacart Anthony <anthony_hacart@ens.univ-artois.fr>
2019-01-25 10:47:19 +0100

    question 10-a

--- src/Controller/ParierController.php
+++ src/Controller/ParierController.php
@@ -5,15 +5,35 @@ namespace App\Controller;
 use App\Entity\Bet;
 use App\Entity\Game;
 use App\Form\BetType;
+use App\Repository\BetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+ * @IsGranted("ROLE_USER")
+ */
 class ParierController extends AbstractController
 {
     /**
-     * @Route("/parier/{id}", name="parier_match", methods="GET")
+     * @Route("/parier/voir/{id}", name="voir_pari_match", methods="GET")
+     */
+    public function voirPari(Game $game, BetRepository $betRepository){
+        $bet = $betRepository->findOneBy(['game' => $game, 'user' => $this->getUser()]);
+
+        if(is_null($bet))
+            return $this->redirectToRoute('parier_match', ['id' => $game->getId()]);
+
+
+        return $this->render('parier/show.html.twig', [
+            'bet' => $bet
+        ]);
+    }
+
+    /**
+     * @Route("/parier/{id}", name="parier_match", methods={"GET","POST"})
      */
     public function parier(Game $game, Request $request): Response
     {
@@ -30,7 +50,7 @@ class ParierController extends AbstractController
         }
 
         return $this->render('parier/new.html.twig', [
-            'game' => $bet,
+            'bet' => $bet,
             'form' => $form->createView(),
         ]);
     }
