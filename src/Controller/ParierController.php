ff7373277b96fea6963484ee8d4c2a06e62499c8
Hacart Anthony <anthony_hacart@ens.univ-artois.fr>
2019-01-25 10:15:49 +0100

    question 9a

--- src/Controller/ParierController.php
+++ src/Controller/ParierController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Bet;
 use App\Entity\Game;
+use App\Form\BetType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +15,23 @@ class ParierController extends AbstractController
     /**
      * @Route("/parier/{id}", name="parier_match", methods="GET")
      */
-    public function parier(Game $game): Response
+    public function parier(Game $game, Request $request): Response
     {
-        return $this->render('home/match_show.html.twig', ['game' => $game]);
+        $bet = new Bet();
+        $form = $this->createForm(BetType::class, $bet, ['game' => $game]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($bet);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('matchs');
+        }
+
+        return $this->render('parier/new.html.twig', [
+            'game' => $bet,
+            'form' => $form->createView(),
+        ]);
     }
 }
