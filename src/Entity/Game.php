79a5810e5ed311ab643c569e7b6f7d9a79f731bd
pierre-deve <pierre.fievet1@ens.univ-artois.fr>
2019-01-25 09:22:56 +0100

    question 3-

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -3,9 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\GameRepository")
+ * @UniqueEntity(fields="teamA", message="TeamA déjà utilisé")
+ * @UniqueEntity(fields="teamB", message="TeamB déjà utilisé")
  */
 class Game
 {
