6d126c64c9e4cfcaa2de50043b7cf199b04d3b5e
giovanni_loope <giovanni_loope@ens.univ-artois.fr>
2019-01-25 10:16:26 +0100

    question 7

--- src/Form/Type/ScoreType.php
+++ src/Form/Type/ScoreType.php
@@ -13,7 +13,8 @@ class ScoreType extends AbstractType
         $resolver->setDefaults([
             'choices' => [
                 0,1,2,3,4,5,6,7,8,9,10,11,12
-            ]
+            ],
+            'attr' => ['onChange' => 'changeColor(this.value)']
         ]);
 
     }
