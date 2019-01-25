f582697016c35a6e74f3167f1bc1a9852bf037db
AlexandreHembert <alexandre-h@hotmail.fr>
2019-01-25 10:42:17 +0100

    question 6 ajout du champ 0

--- src/Form/Type/ScoreType.php
+++ src/Form/Type/ScoreType.php
@@ -9,6 +9,7 @@ class ScoreType extends AbstractType
     {
         $resolver->setDefaults([
             'choices' => [
+                '0' => '0',
                 '1' => '1',
                 '2' => '2',
                 '3' => '3',
