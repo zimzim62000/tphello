aa3e9c68d9c7c84c3c126aa523cf207545e29d3b
Hacart Anthony <anthony_hacart@ens.univ-artois.fr>
2019-03-28 09:18:24 +0100

    question 1.3

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/product")
@@ -17,6 +18,8 @@ class ProductController extends AbstractController
 {
     /**
      * @Route("/", name="product_index", methods={"GET"})
+     *
+     * @IsGranted("ROLE_USER")
      */
     public function index(ProductRepository $productRepository): Response
     {
@@ -27,6 +30,8 @@ class ProductController extends AbstractController
 
     /**
      * @Route("/new", name="product_new", methods={"GET","POST"})
+     *
+     * @IsGranted("ROLE_ADMIN")
      */
     public function new(Request $request): Response
     {
@@ -60,6 +65,8 @@ class ProductController extends AbstractController
 
     /**
      * @Route("/{id}/edit", name="product_edit", methods={"GET","POST"})
+     *
+     * @IsGranted("ROLE_ADMIN")
      */
     public function edit(Request $request, Product $product): Response
     {
@@ -82,6 +89,8 @@ class ProductController extends AbstractController
 
     /**
      * @Route("/{id}", name="product_delete", methods={"DELETE"})
+     *
+     * @IsGranted("ROLE_ADMIN")
      */
     public function delete(Request $request, Product $product): Response
     {
