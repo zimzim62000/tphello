b4ac736e669bb88a14cc13e947a4cf804b906b7f
Hacart Anthony <anthony_hacart@ens.univ-artois.fr>
2019-03-28 09:30:59 +0100

    fix

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -55,11 +55,16 @@ class AppFixtures extends Fixture
             ->setQuantity(44)
             ->setCreatedAt(new \DateTime());
 
+        $manager->persist($produit1);
+
         $userProduct1 = new UserProduct();
         $userProduct1->setProduct($produit1)
             ->setUser($user1)
             ->setQuantity(31)
             ->setCreatedAt(new \DateTime());
+
+        $manager->persist($userProduct1);
+
 
         $userProduct11 = new UserProduct();
         $userProduct11->setProduct($produit1)
@@ -67,11 +72,17 @@ class AppFixtures extends Fixture
             ->setQuantity(13)
             ->setCreatedAt(new \DateTime());
 
+        $manager->persist($userProduct11);
+
+
         $produit2 = new Product();
         $produit2->setName('Pieces')
             ->setPrice(4.99)
             ->setQuantity(100)
             ->setCreatedAt(new \DateTime());
+
+        $manager->persist($produit2);
+
 
         $userProduct2 = new UserProduct();
         $userProduct2->setProduct($produit2)
@@ -79,11 +90,16 @@ class AppFixtures extends Fixture
             ->setQuantity(100)
             ->setCreatedAt(new \DateTime());
 
+        $manager->persist($userProduct2);
+
+
         $produit3 = new Product();
         $produit3->setName('Mini voitures')
             ->setPrice(4.99)
             ->setQuantity(14)
             ->setCreatedAt(new \DateTime());
+
+        $manager->persist($produit3);
 
         $userProduct3 = new UserProduct();
         $userProduct3->setProduct($produit3)
@@ -91,11 +107,16 @@ class AppFixtures extends Fixture
             ->setQuantity(6)
             ->setCreatedAt(new \DateTime());
 
+        $manager->persist($userProduct3);
+
         $produit4 = new Product();
         $produit4->setName('Maillots')
             ->setPrice(85)
             ->setQuantity(5)
             ->setCreatedAt(new \DateTime());
+
+        $manager->persist($produit4);
+
 
         $userProduct4 = new UserProduct();
         $userProduct4->setProduct($produit4)
@@ -103,17 +124,26 @@ class AppFixtures extends Fixture
             ->setQuantity(5)
             ->setCreatedAt(new \DateTime());
 
+        $manager->persist($userProduct4);
+
+
         $produit5 = new Product();
         $produit5->setName('Vestes')
             ->setPrice(110)
             ->setQuantity(14)
             ->setCreatedAt(new \DateTime());
 
+        $manager->persist($produit5);
+
+
         $userProduct5 = new UserProduct();
         $userProduct5->setProduct($produit5)
             ->setUser($user3)
             ->setQuantity(5)
             ->setCreatedAt(new \DateTime());
+
+        $manager->persist($userProduct5);
+
 
 
         $manager->flush();
