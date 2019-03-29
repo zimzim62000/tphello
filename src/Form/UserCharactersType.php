89af1da4cc7570bb2742d4cdb9e89e9cbd7dc9bf
feykrokhiin <crossmarian59@gmail.com>
2019-03-29 14:01:26 +0100

    'Q3.1'

--- src/Form/UserCharactersType.php
+++ src/Form/UserCharactersType.php
@@ -5,10 +5,19 @@ namespace App\Form;
 use App\Entity\UserCharacters;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class UserCharactersType extends AbstractType
 {
+    private $token;
+
+    public function __construct(TokenStorageInterface $token)
+    {
+        $this->token = $token;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -25,5 +34,26 @@ class UserCharactersType extends AbstractType
         $resolver->setDefaults([
             'data_class' => UserCharacters::class,
         ]);
+    }
+
+    public function preSetData(FormEvent $event) {
+
+        $form = $event->getForm();
+        $userCharacter = $event->getData();
+
+        if(in_array('ROLE_ADMIN', $this->token->getToken()->getUser()->getRoles()) !== true) {
+
+            $userCharacter->setDefaultCharacter(false);
+            $form->remove('default');
+
+            $userCharacter->setFavorite(false);
+            $form->remove('favorite');
+
+            $userCharacter->setCreatedAt(new \DateTime());
+            $form->remove('createdAt');
+
+            $userCharacter->setUser($this->token->getToken()->getUser());
+            $form->remove('user');
+        }
     }
 }
