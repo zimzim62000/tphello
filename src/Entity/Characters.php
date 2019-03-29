131e9266c43e86ec334aca21da97e40c2308e949
Amaury Beauchamp <amaury_beauchamp@ens.univ-artois.fr>
2019-03-29 14:14:24 +0100

    question 2.2

--- src/Entity/Characters.php
+++ src/Entity/Characters.php
@@ -4,9 +4,11 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CharactersRepository")
+ * @UniqueEntity("name")
  */
 class Characters
 {
