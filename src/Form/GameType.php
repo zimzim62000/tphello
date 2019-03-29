eef44054b6ee15e1697f6f6fe2d4566c134ed529
Amaury Beauchamp <amaury_beauchamp@ens.univ-artois.fr>
2019-03-29 15:36:11 +0100

    question 4.1

--- src/Form/GameType.php
+++ src/Form/GameType.php
@@ -5,11 +5,24 @@ namespace App\Form;
 use App\Entity\Game;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class GameType extends AbstractType
 {
+    private $securityChecker;
+    private $token;
+
+    public function __construct(AuthorizationCheckerInterface $securityChecker, TokenStorageInterface $token)
+    {
+        $this->securityChecker = $securityChecker;
+        $this->token = $token;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -21,6 +34,22 @@ class GameType extends AbstractType
             ->add('userCharacters')
             ->add('submit', SubmitType::class )
         ;
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            array($this, 'preSetData')
+        );
+    }
+
+    public function preSetData(FormEvent $event)
+    {
+        $form = $event->getForm();
+        $game = $event->getData();
+
+        if($game->getId() === null) {
+            $form->add('submit', SubmitType::class, ['label_format' => 'Créer une partie ']);
+        }
+        $form->add('submit', SubmitType::class, ['label_format' => 'Modifier la partie']);
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
