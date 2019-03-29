7736ada2235ecbaa4f8f822b047fc8104c44007e
giovanni_loope <giovanni_loope@ens.univ-artois.fr>
2019-03-29 13:50:33 +0100

    Question 3.1

--- src/Form/UserCharactersType.php
+++ src/Form/UserCharactersType.php
@@ -5,10 +5,23 @@ namespace App\Form;
 use App\Entity\UserCharacters;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class UserCharactersType extends AbstractType
 {
+    private $securityChecker;
+    private $token;
+
+    public function __construct(AuthorizationCheckerInterface $securityChecker, TokenStorageInterface $token)
+    {
+        $this->securityChecker = $securityChecker;
+        $this->token = $token;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -18,6 +31,11 @@ class UserCharactersType extends AbstractType
             ->add('user')
             ->add('characters')
         ;
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            array($this, 'preSetData')
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -26,4 +44,26 @@ class UserCharactersType extends AbstractType
             'data_class' => UserCharacters::class,
         ]);
     }
+
+
+    public function preSetData(FormEvent $event)
+    {
+        $form = $event->getForm();
+        $userCharacters = $event->getData();
+
+        if($this->securityChecker->isGranted('ROLE_USER') === true && $this->securityChecker->isGranted('ROLE_ADMIN') === false){
+            $userCharacters->setUser($this->token->getToken()->getUser());
+            $userCharacters->setDefaultCharacter(false);
+            $userCharacters->setFavorite(false);
+            $userCharacters->setCreatedAt(new \DateTime('now'));
+
+            $form->remove('user');
+            $form->remove('defaultCharacter');
+            $form->remove('favorite');
+            $form->remove('createdAt');
+        }
+    }
 }
+
+// Modification du formulaire UserCharactersType:new pour que seul le champs Character sois visible
+// ( Default = false, Favorite = false, date = now et user set) mais les champs doivent toujours etre disponible pour les ROLE_ADMIN
