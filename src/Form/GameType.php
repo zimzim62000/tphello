3dc3638178262579ae3a35e714121272173e534e
Hacart Anthony <anthony_hacart@ens.univ-artois.fr>
2019-03-29 14:40:32 +0100

    question 4.1

--- src/Form/GameType.php
+++ src/Form/GameType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Game;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
@@ -19,8 +21,22 @@ class GameType extends AbstractType
             ->add('reanimation')
             ->add('damage')
             ->add('userCharacters')
-            ->add('submit', SubmitType::class )
+            ->add('submit', SubmitType::class , ['label' => 'Modifier une partie'])
         ;
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            array($this, 'preSetData')
+        );
+    }
+
+    public function preSetData(FormEvent $event)
+    {
+        $form = $event->getForm();
+        $game = $event->getData();
+        if ($game->getId() === null) {
+            $form->remove('submit')->add('submit',SubmitType::class, ['label' => 'Créer une partie']);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
