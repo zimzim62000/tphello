42616bcf795f074fdfeaf389f368f3b840c07d84
Alexis Delehaye <alexis_delehaye@ens.univ-artois.fr>
2019-03-29 14:53:18 +0100

    question 3.1

--- src/Form/UserCharactersType.php
+++ src/Form/UserCharactersType.php
@@ -5,10 +5,22 @@ namespace App\Form;
 use App\Entity\UserCharacters;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class UserCharactersType extends AbstractType
 {
+    private $securityChecker;
+    private $token;
+
+    public function __construct(AuthorizationCheckerInterface $securityChecker, TokenStorageInterface $token)
+    {
+        $this->securityChecker = $securityChecker;
+        $this->token = $token;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -18,6 +30,11 @@ class UserCharactersType extends AbstractType
             ->add('user')
             ->add('characters')
         ;
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            array($this, 'preSetData')
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -25,5 +42,23 @@ class UserCharactersType extends AbstractType
         $resolver->setDefaults([
             'data_class' => UserCharacters::class,
         ]);
+    }
+
+    public function preSetData(FormEvent $event)
+    {
+
+        $form = $event->getForm();
+        $userCharacter = $event->getData();
+        $userCharacter->setDefaultCharacter(false);
+        $userCharacter->setFavorite(false);
+        $userCharacter->setCreatedAt(new \DateTime());
+        $userCharacter->setUser($this->token->getToken()->getUser());
+
+        if($this->securityChecker->isGranted('ROLE_ADMIN') === false){
+            $form->remove("favorite");
+            $form->remove("createdAt");
+            $form->remove("defaultCharacter");
+            $form->remove("user");
+        }
     }
 }
