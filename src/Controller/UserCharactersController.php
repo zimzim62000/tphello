46c289a1edc6ace49dc1037ad3444b7979db9672
Hacart Anthony <anthony_hacart@ens.univ-artois.fr>
2019-03-29 12:47:32 +0100

    question 1.1

--- src/Controller/UserCharactersController.php
+++ src/Controller/UserCharactersController.php
@@ -12,9 +12,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/user-characters")
+ *
+ * @IsGranted("ROLE_USER")
  */
 class UserCharactersController extends AbstractController
 {
