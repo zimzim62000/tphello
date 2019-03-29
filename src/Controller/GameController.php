b4f428453a49e194dcb04e3544d60166b0cab578
AlexandreHembert <alexandre-h@hotmail.fr>
2019-03-29 15:51:16 +0100

    Question 4.2

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Game;
+use App\Entity\UserCharacters;
 use App\Form\GameType;
 use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,12 +27,13 @@ class GameController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="game_new", methods={"GET","POST"})
+     * @Route("/new/{gameT}", name="game_new", methods={"GET","POST"},defaults={"gameT"=null})
      */
-    public function new(Request $request): Response
+    public function new(Request $request,UserCharacters $gameT=null): Response
     {
         $game = new Game();
-        $form = $this->createForm(GameType::class, $game);
+        $form = $this->createForm(GameType::class, $game,['gameT'=>$gameT]);
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
