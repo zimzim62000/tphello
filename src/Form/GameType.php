7a023b87777f28d4f9da3778ad0769362ff5421f
giovanni_loope <giovanni_loope@ens.univ-artois.fr>
2019-03-29 14:42:42 +0100

    Question 4.3

--- src/Form/GameType.php
+++ src/Form/GameType.php
@@ -12,8 +12,12 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class GameType extends AbstractType
 {
+    private $userCharacters;
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $this->userCharacters = $options['userCharacters'];
+
         $builder
             ->add('createdAt')
             ->add('position')
@@ -34,6 +38,7 @@ class GameType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Game::class,
+            'userCharacters' => null,
         ]);
     }
 
@@ -44,8 +49,26 @@ class GameType extends AbstractType
 
         if($game->getId() === null){
             $form->add('submit', SubmitType::class, ['label_format' => 'Créer une partie']);
+            $game->setCreatedAt(new \DateTime('now'))
+                ->setPosition(0)
+                ->setAssassination(0)
+                ->setReanimation(0)
+                ->setDamage(0)
+                ->setUserCharacters($this->userCharacters);
+
+            $form->remove("createdAt")
+                ->remove("position")
+                ->remove("assassination")
+                ->remove("reanimation")
+                ->remove("damage")
+                ->remove("userCharacters");
+
         }else{
             $form->add('submit', SubmitType::class, ['label_format' => 'Modifier la partie']);
         }
+
+
+        // Modification de GameType pour lors du clic sur "Créer une partie",
+        // le formulaire sois automatiquement setter ( Date = now, tous les champs à 0 et userCharacter setter )
     }
 }
